85e127f8856e01e38dfae7a08f1b5fa3143d516a
Vladka Vrana <hovka@seznam.cz>
2023-10-21 16:02:08 +0200

    oprava a dalsi pokus dvou verzi jmena

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const jmeno = "Vladka"
 const prijmeni = "Vrana"
 
-document.body.innerHTML = "<h1>" + jmeno + "</h1>"
-document.body.innerHTML = "<h2>" + jmeno + "</h2>"
+document.body.innerHTML += "<h1>" + jmeno + "</h1>"
+document.body.innerHTML += "<h2>" + jmeno + "</h2>"
